e9cfb6278b1e86ee24005b9ceb48ca2cf8d54c9c
Adam Pah <adamrpah@gmail.com>
2017-10-02 10:55:06 -0500

    adding in paper settings for figure creation

--- gale/figures/settings.py
+++ gale/figures/settings.py
@@ -1,12 +1,14 @@
 import seaborn as sns
 import matplotlib as mpl
+import matplotlib.pyplot as plt
 
+#set the font
 font = {'size'   : 12}
 mpl.rc('font', **font)
 inset_font_size = 22
-
+#Start up seaborn
 sns.set_context('talk')
 sns.set_style({'font.family': 'sans-serif', 'font.sans-serif': 'Arial Narrow', 'font.size': 18})
-
+#Create the fontdict for use in 
 fontdict = {'size' : 22, 'weight':'bold'}
 
