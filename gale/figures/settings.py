06b399a9faf2a325e7418f014b1603dd7154de06
Adam Pah <adamrpah@gmail.com>
2017-10-02 10:38:08 -0500

    adding figure settings

---
+++ gale/figures/settings.py
@@ -0,0 +1,12 @@
+import seaborn as sns
+import matplotlib as mpl
+
+font = {'size'   : 12}
+mpl.rc('font', **font)
+inset_font_size = 22
+
+sns.set_context('talk')
+sns.set_style({'font.family': 'sans-serif', 'font.sans-serif': 'Arial Narrow', 'font.size': 18})
+
+fontdict = {'size' : 22, 'weight':'bold'}
+
