4fa6356e20e95bb98ede6d529330a09137251fc9
Adam Pah <adamrpah@gmail.com>
2019-10-23 16:16:44 -0500

    Standard commit fired from the brain

--- gale/figures/settings.py
+++ gale/figures/settings.py
@@ -8,7 +8,7 @@ mpl.rc('font', **font)
 inset_font_size = 22
 #Start up seaborn
 sns.set_context('talk')
-sns.set_style({'font.family': 'sans-serif', 'font.sans-serif': 'Arial Narrow', 'font.size': 18})
+sns.set_style({'font.size': 18})
 #Create the fontdict for use in 
 fontdict = {'size' : 22, 'weight':'bold'}
 
